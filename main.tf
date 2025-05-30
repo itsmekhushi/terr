dd610005d49e7e8d75e1e23e7601a5c89603289e
Ubuntu <ubuntu@ip-172-31-15-44.ap-south-1.compute.internal>
2025-05-30 05:34:43 +0000

    Update terraform script

--- main.tf
+++ main.tf
@@ -1,17 +1,25 @@
 provider "aws" {
   region = "ap-south-1"
 }
-
 resource "aws_instance" "example" {
-  ami           = "ami-0026e3c171092945f" preferred AMI ID
+  ami           = "ami-0f58b397bc5c1f2e8"  
   instance_type = "t2.micro"
+  key_name      = "terr"
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install apache2 -y
+              echo "<h1>Hello from Terraform EC2 which is connected with ssh agent</h1>" | sudo tee /var/www/html/index.html
+              sudo systemctl enable apache2
+              sudo systemctl start apache2
+            EOF
+
+          
+              
+           
 
   tags = {
-    Name = "JenkinsTerraformEC2"
+    Name = "example-instance"
   }
-  user_data = <<-EOF
-    #!/bin/bash
-    echo "it works on ssh connection" > /home/ec2-user/agent_info.txt
-  EOF
 }
 
